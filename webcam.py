36b5cd5ac8ecfc508084a05666d4bf623e814684
vitamingoon <vitamingoon@gmail.com>
2023-11-13 23:16:49 +0900

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,60 +1,70 @@
+import streamlit as st
 import cv2
 import tensorflow as tf
 import numpy as np
+import io
 
-def load_model(model_path):
-    model = tf.keras.models.load_model(model_path)
-    return model
+# Load the TensorFlow SavedModel
+model_path = "./saved_model_directory"  # Change to the path where you saved the converted model
+model = tf.saved_model.load(model_path)
 
+# Preprocess the image
 def preprocess_image(frame):
-    # Resize the image to the size expected by the model
     resized_frame = cv2.resize(frame, (224, 224))
-    # Preprocess the image (normalize pixel values to be between 0 and 1)
     normalized_frame = resized_frame / 255.0
-    # Expand dimensions to create a batch (model expects a batch of images)
     input_array = np.expand_dims(normalized_frame, axis=0)
     return input_array
 
-def main():
-    # Load the Teachable Machine model
-    model_path = "path/to/your/model"  # Replace with the path to your model file
-    model = load_model(model_path)
+# Make predictions using the model
+def predict_image(frame):
+    input_array = preprocess_image(frame)
+    result = model(input_array)
+    return result
 
-    # Open the webcam
-    cap = cv2.VideoCapture(0)
+# Streamlit app
+st.title("Teachable Machine Image Classifier")
 
-    if not cap.isOpened():
-        print("Error: Could not open webcam.")
-        return
+# Counter for uploaded files
+file_count = st.session_state.get("file_count", 0)
 
-    while True:
+# File uploader for images
+uploaded_image_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+
+# Counter for captured webcam images
+captured_image_count = st.session_state.get("captured_image_count", 0)
+
+# Open the webcam
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    st.error("Error: Could not open webcam.")
+else:
+    st.write("Webcam is on. Press 'Capture' to capture an image.")
+
+    # Button to capture an image
+    if st.button('Capture'):
         # Read a single frame from the webcam
         ret, frame = cap.read()
 
         if not ret:
-            print("Error: Could not read frame.")
-            break
+            st.error("Error: Could not read frame.")
+        else:
+            # Increment the captured image count
+            captured_image_count += 1
+            st.session_state.captured_image_count = captured_image_count
 
-        # Preprocess the captured frame
-        input_array = preprocess_image(frame)
+            # Display the captured image
+            st.image(frame, caption=f"Captured Image {captured_image_count}", use_column_width=True)
 
-        # Make predictions using the model
-        predictions = model.predict(input_array)
+            # Make predictions
+            predictions = predict_image(frame)
 
-        # Get the class with the highest probability
-        predicted_class = np.argmax(predictions)
+            # Display predictions
+            st.write("Class probabilities:")
+            for i, prob in enumerate(predictions[0]):
+                st.write(f"Class {i}: {prob:.4f}")
 
-        # Display the result on the frame
-        cv2.putText(frame, f"Class: {predicted_class}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        cv2.imshow("Teachable Machine", frame)
-
-        # Break the loop if 'q' key is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Release the webcam
-    cap.release()
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
+# Display the total file count
+st.write(f"Total Uploaded Files: {file_count}")
+# Display the total captured image count
+st.write(f"Total Captured Images: {captured_image_count}")
